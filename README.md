51691359bbe8dbe2ccdf79f106dbbb2dfcc5d2f4
Felix Lueth <felix.lueth@t-online.de>
2020-04-12 13:18:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# COVID-19 Plots
+
+## Data
+Data Source:
+https://opendata.ecdc.europa.eu/covid19/casedistribution/csv
+
+The file needs to be renamed(`data_all.csv`) and moved into the `data/` directory. 
+
+## Installation and usage
+This script works with python 3.7.4
+```bash
+# Install matplotlib
+pip install -r requirements.txt
+
+# Example single plot
+python single_plot.py USA
+```
+## Output
+
+The output can be found in `output/`
---
+++ single_plot.py
@@ -0,0 +1,42 @@
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+import sys
+
+def read_file(country):
+  data = []
+  with open('data/data_all.csv') as file:
+    for line in file:
+      if country in line:        
+        data.append(line.strip().split(','))
+
+  return reversed(data)
+
+def create_plot(data, country):
+  dates = []
+  cases = []
+  for entry in data:
+    dates.append(entry[0])
+    cases.append(entry[4])
+
+  # Change cases from string to integer
+  cases = list(map(int, cases))
+
+  plt.figure(figsize=(20, 10), dpi=150)
+  plt.plot(tuple(dates), cases)
+  plt.title('Dailey Cases '+ country)
+  plt.xlabel('Date')
+  plt.ylabel('Cases')
+  plt.tick_params(axis='x', rotation=-90)
+
+  plt.savefig('output/daily_cases_' + country + '.png')
+
+
+def main():
+  COUNTRY = sys.argv[1]
+
+  country_data_raw = read_file(COUNTRY)
+  # country_data_prun = prun_data(country_data_raw)
+  create_plot(country_data_raw, COUNTRY)
+
+main()
