43fb8d6ae198a161749e94aa0acea7aef5f0988c
Felix Lueth <felix.lueth@t-online.de>
2020-04-14 12:26:36 +0200

    Adds double plot

---
+++ double_plot.py
@@ -0,0 +1,52 @@
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+import sys
+
+def get_data(country, index):
+  data_raw = []
+  data = []
+  with open('data/data_all.csv') as file:
+    for line in file:
+      if country in line:
+        data_raw.append(line.strip().split(','))
+        
+  for entry in data_raw:
+    if index == 0:
+      data.append(entry[index])
+    
+    if index == 4:
+      data.append(entry[index])
+      
+  return reversed(data)
+
+def create_plot(dataset_one, dataset_two, dates, country_one, country_two):
+  days = [x for x in range(0, len(dates))]
+  plt.figure(figsize=(20, 10), dpi=150)
+  plt.plot(days, dataset_one, days, dataset_two)
+  # plt.plot(tuple(dates), dataset_two)
+  plt.title('Daily Cases  ' + country_one + ' and ' + country_two )
+  plt.xlabel('Date')
+  plt.ylabel('Cases')
+  plt.tick_params(axis='x', rotation=-90)
+
+  plt.savefig('output/dailey_cases_' + country_one + '_' + country_two + '.png')
+  plt.show()
+
+
+def main():
+  COUNTRY_ONE = sys.argv[1]
+  COUNTRY_TWO = sys.argv[2]
+
+  dataset_one = list(map(int, get_data(COUNTRY_ONE, 4)))
+  dataset_two = list(map(int, get_data(COUNTRY_TWO, 4)))
+
+  print(len(dataset_one))
+  print(len(dataset_two))
+
+  dates = list(map(str,get_data(COUNTRY_ONE, 0)))
+
+  create_plot(dataset_one, dataset_two, dates, COUNTRY_ONE, COUNTRY_TWO)
+
+
+main()
