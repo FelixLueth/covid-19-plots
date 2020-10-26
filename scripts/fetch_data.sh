5a53ec3247df3e543158340f32827f3db003627f
Felix Lueth <felix.lueth@t-online.de>
2020-10-26 09:59:37 +0100

    Modifies fetch script

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,2 +1,6 @@
 wget https://opendata.ecdc.europa.eu/covid19/casedistribution/csv
+
 mv csv data/data_all.csv
+
+wget https://opendata.ecdc.europa.eu/covid19/casedistribution/csv 
+mv csv 20200610
--- src/single_plot.py
+++ src/single_plot.py
@@ -1,7 +1,8 @@
 import datetime
 import matplotlib
 import matplotlib.pyplot as plt
-import sys
+import argparse
+
 
 def read_file(country):
   data = []
@@ -13,10 +14,13 @@ def read_file(country):
   return reversed(data)
 
 
-def create_plot(data, country):
+def create_plot(data, country, show_plot):
   dates = []
   cases = []
   deaths = []
+  accumulation_cases = [0]
+  accumulation_deaths = [0]
+
   for entry in data:
     dates.append(entry[0])
     cases.append(entry[4])
@@ -26,9 +30,15 @@ def create_plot(data, country):
   cases = list(map(int, cases))
   deaths = list(map(int, deaths))
 
+  for x in range(1, len(cases)):
+    accumulation_cases.append(accumulation_cases[x-1] + cases[x])
+    accumulation_deaths.append(accumulation_deaths[x-1] + deaths[x])
+
   plt.figure(figsize=(20, 10), dpi=150)
   plt.plot(tuple(dates), cases, label='cases')
   plt.plot(tuple(dates), deaths, label='deaths')
+  # plt.plot(tuple(dates), accumulation_cases, label='accumulated cases')
+  # plt.plot(tuple(dates), accumulation_deaths, label='accumulated deaths')
   plt.title('Dailey Cases '+ country)
   plt.xlabel('Date')
   plt.ylabel('Cases')
@@ -37,14 +47,28 @@ def create_plot(data, country):
   plt.tick_params(axis='x', rotation=-90)
 
   plt.savefig('output/daily_cases_' + country + '.png')
-  plt.show()
+  
+  if show_plot == True:
+    print('show plot...')
+    plt.show()
 
 
 def main():
-  COUNTRY = sys.argv[1]
+  
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', '--show', help='Specifies whether the plot will be displayed or not.', action='store_true')
+  parser.add_argument('country', help='Specifies the country for the plot')
+  args = parser.parse_args()
+  
+  
+  COUNTRY = args.country
+  show_plot = False
+  
+  if args.show:
+    show_plot = True
 
   country_data_raw = read_file(COUNTRY)
-  create_plot(country_data_raw, COUNTRY)
+  create_plot(country_data_raw, COUNTRY, show_plot)
 
 
 main()
